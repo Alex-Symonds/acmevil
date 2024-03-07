20d65137839dd0a270ef2b27aa09c56d1a43fa95
Alex Symonds <alex-symonds@gmail.com>
2024-03-07 17:02:06 +0000

    fix: safari text colour issue

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,7 +13,7 @@ import { CloseButtonComponent } from '../close-button/close-button.component';
       (click)="openNavBar()"
     >
       <svg preserveAspectRatio="none" width="36" height="36" version="1.1" viewBox="0 0 9.52 9.53" xmlns="http://www.w3.org/2000/svg"><g><rect x=".00176" y=".7" width="9.52" height="1.85" rx="1.06" ry="1.62"/><rect x="-4.73e-9" y="6.97" width="9.52" height="1.85" rx="1.06" ry="1.62"/><rect y="3.84" width="9.52" height="1.85" rx="1.06" ry="1.62"/></g></svg>
-      <span>menu</span>
+      <span class="burgerLabel">menu</span>
     </button>
     <div class="navAndCTA">
       <div class="closeButtonWrapper">
